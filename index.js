4d89571da68adeeabaaf9ee7d3e30d15e3db1bbc
oleg <litwinenkooleg371@gmail.com>
2021-04-30 17:26:52 +0300

    работает все кроме вывода контакта по id

---
+++ index.js
@@ -0,0 +1,28 @@
+const { listContacts, getContactById, removeContact, addContact } = require('./contacts')
+const argv = require('yargs').argv;
+
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      listContacts().then(console.table)
+      break;
+
+    case 'get':
+      getContactById().then(console.log)
+      break;
+
+    case 'add':
+      addContact(name, email, phone)
+      break;
+
+    case 'remove':
+      removeContact(id)
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
